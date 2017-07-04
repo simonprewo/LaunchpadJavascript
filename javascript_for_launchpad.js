651bdeee6ea389e99ee902e05c2282d9be5fb4cc
Simon Prewo <simon-marlon.prewo@telekom.de>
2017-07-04 14:10:16 +0200

    Code Refactoring

--- javascript_for_launchpad.js
+++ javascript_for_launchpad.js
@@ -12,9 +12,10 @@ var handlingOTConLaunchpad = {
 			}
 			setTimeout(function(){}, waitExist * 10);
 		}
+		handlingOTConLaunchpad.cleanUpInitialElement(initialOTCElement);
 		for (var counterForApps = 0; counterForApps < data.content.length; counterForApps++) {
 			var currentApp = data.content[counterForApps];
-			if (currentApp.applicationId != null && currentApp.applicationId == filterapplicationid){
+			if (currentApp.applicationId != null && currentApp.applicationId == filterapplicationid) {
 				var tcappdchannel = "";
 				if (currentApp.partner === "DEUTSCHE"){
 					tcappdchannel= "ADT-TDG";
@@ -24,15 +25,11 @@ var handlingOTConLaunchpad = {
 				var tcotcurl = tcotcbasisurl+tcappdchannel+"&xdomain_id="+currentApp.companyEntitlementExternalVendorIdentifier;
 				if (firstitemdet == false){
 					handlingOTConLaunchpad.cloneLaunchPadItem(initialOTCElement, currentApp.assignUrl, currentApp.manageUrl, currentApp.companyEntitlementExternalVendorIdentifier, tcotcurl);
+				} else if (tcappdchannel === "ADT-TDG") {
+					initialOTCElement.find("div.status-title").text(currentApp.companyEntitlementExternalVendorIdentifier);
 				} else {
-					var tentxtfirst = currentApp.companyEntitlementExternalVendorIdentifier;
-					initialOTCElement.find("div.status-title").text(tentxtfirst);
-					initialOTCElement.find("div.status-title").css("font-size", "15px");
-					initialOTCElement.find("a.myapps-assign-users").parent().remove();
-					handlingOTConLaunchpad.removeStringStarten(initialOTCElement.find("div.myapps-image"));
-					initialOTCElement.find("div.myapp-body").click(function(e) {
-						window.open(tcotcurl, '_blank');
-					});
+					initialOTCElement.find("div.status-title").text(currentApp.companyEntitlementExternalVendorIdentifier);
+					handlingOTConLaunchpad.setOtcLink(initialOTCElement, tcotcurl);
 				}
 				firstitemdet = false;
 			}
@@ -45,15 +42,7 @@ var handlingOTConLaunchpad = {
 		clonedItem.find("div.myapp-body").click(function(e) {
 			window.open(otcLink, '_blank');
 		});
-		clonedItem.find("a.myapps-settings").click(function(event) {
-			$(this).closest("div.myapps-item").find("div.myapps-settings-menu").toggle();
-			if ( clonedItem.find("a.myapps-assign-users").length ) {
-				$(this).closest("div.myapps-item").find("div.myapps-settings-menu").css({top: 108, left: 91, position:'absolute'});
-			} else {
-				$(this).closest("div.myapps-item").find("div.myapps-settings-menu").css({top: 148, left: 91, position:'absolute'});
-			}
-			event.stopPropagation();
-		});
+		handlingOTConLaunchpad.setOtcLink(clonedItem, otcLink);
 		if ( clonedItem.find("a.myapps-assign-users").length ) {
 			clonedItem.find("a.myapps-assign-users").attr("href", assignUsersURL);
 		}
@@ -62,10 +51,31 @@ var handlingOTConLaunchpad = {
 		clonedItem.appendTo( $("#myappsCollectionView").last()).show();
 	},
 	
+	setOtcLink: function(otcItem, otcLink) {
+		otcItem.find("div.myapp-body").click(function(e) {
+			window.open(otcLink, '_blank');
+		});
+		otcItem.find("a.myapps-settings").click(function(event) {
+			$(this).closest("div.myapps-item").find("div.myapps-settings-menu").toggle();
+			if ( otcItem.find("a.myapps-assign-users").length ) {
+				$(this).closest("div.myapps-item").find("div.myapps-settings-menu").css({top: 108, left: 91, position:'absolute'});
+			} else {
+				$(this).closest("div.myapps-item").find("div.myapps-settings-menu").css({top: 148, left: 91, position:'absolute'});
+			}
+			event.stopPropagation();
+		});
+	},
+	
 	removeStringStarten: function(elementToRemoveFromTitle) {
 		var title = elementToRemoveFromTitle.attr("title");
 		title = title.replace('starten','');
 		elementToRemoveFromTitle.attr("title", title)
+	},
+	
+	cleanUpInitialElement: function(initialOTCElement) {
+		initialOTCElement.find("div.status-title").css("font-size", "15px");
+		initialOTCElement.find("a.myapps-assign-users").parent().remove();
+		handlingOTConLaunchpad.removeStringStarten(initialOTCElement.find("div.myapps-image"));
 	}
 	
 };
